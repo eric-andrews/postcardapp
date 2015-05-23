dd48aad4767e4b29c3863e9568f25f0001cbf17b
Eric Andrews <ericandrews@Erics-MacBook-Pro.local>
2015-05-23 10:27:12 -0400

    added a comment to test commit

--- Postcard2/ViewController.swift
+++ Postcard2/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func sendMailButtonPressed(sender: UIButton) {
+        // Adding a comment here to test commit
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
